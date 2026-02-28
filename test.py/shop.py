8954afd9b75f31feb6b4ccf1b63cdf0ecc362a04
jun <salt.pepper0703@gmail.com>
2026-02-28 17:41:25 +0900

    shop.pyを割引機能追加前の状態にロールバック

--- test.py/shop.py
+++ test.py/shop.py
@@ -1,14 +1,16 @@
+# 名前を教えてもらう(入力待ち状態になります)
+user_name = input("あなたの名前を教えて下さい: ")
 
-# --- 自由入力の割引キャンペーン ---
-print("ーーー割引キャンペーン実施中!ーーー")
-price = int(input("商品の単価を入力して下さい: ")) 
-count = int(input("個数を入力して下さい: "))
+#挨拶を表示します
+print("こんにちは、" + user_name + "さん！")
 
-total = price * count
+#好きなリンゴの数を聞く
+# inputで入ってきたものは「文字」なので、int()を使って「数字」に変換します
+apple_count = input("りんごは何個買いますか？: " )
+apple_count = int(apple_count)
 
-# 2000円以上ならさらに割引
-if total >= 2000:
-    print("2000円以上なので 10% 割引します!")
-    total = total * 0.9
+#合計金額を計算する(1個100円とします)
+total_price = apple_count * 150
 
-print(f"キャンペーン適用後の合計金額は {int(total)} 円です。")
+#結果を表示します
+print(user_name + "さん、合計は," + str(total_price) + "円ですよ")
